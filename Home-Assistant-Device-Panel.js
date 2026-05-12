5108f7e1bd8e4deab46d0d84fe7b55616e68351d
SmartWay Systems <hollako75@hotmail.com>
2026-05-12 21:09:12 +0300

    Solve HACS Update Issue

--- Home-Assistant-Device-Panel.js
+++ Home-Assistant-Device-Panel.js
@@ -1,4 +1,5 @@
-const cacheQuery = new URL(import.meta.url).search;
+const VERSION = "1.0.7";
+const cacheQuery = new URL(import.meta.url).search || `?v=${VERSION}`;
 
 await import(`./device-card-list-panel.js${cacheQuery}`);
 await import(`./device-map-panel.js${cacheQuery}`);
